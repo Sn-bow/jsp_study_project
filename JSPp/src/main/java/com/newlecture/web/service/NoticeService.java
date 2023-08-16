3080013f96b7ce352ea32c01d21952f702860de5
JHS <JHS@211.58.105.31>
2023-08-16 12:10:58 +0900

    commit test

--- JSPp/src/main/java/com/newlecture/web/service/NoticeService.java
+++ JSPp/src/main/java/com/newlecture/web/service/NoticeService.java
@@ -5,16 +5,38 @@ import java.util.List;
 import com.newlecture.web.entity.Notice;
 
 public class NoticeService {
-
+	// public method
 	public List<Notice> getNoticeList() {
-		return null;
+		return getNoticeList("title", "", 1);
 	}
 
+	// override method
 	public List<Notice> getNoticeList(int page) {
+		return getNoticeList("title", "", page);
+	}
+
+	// override method
+	public List<Notice> getNoticeList(String field, String query, int page) {
 		return null;
 	}
 
-	public List<Notice> getNoticeList(String field, String query, int page) {
+	public int getNoticeCount() {
+		return getNoticeCount("title", "");
+	}
+
+	public int getNoticeCount(String field, String query) {
+		return 0;
+	}
+
+	public Notice getNotice(int id) {
+		return null;
+	}
+
+	public Notice getNextNotice(int id) {
+		return null;
+	}
+
+	public Notice getPrevNotice(int id) {
 		return null;
 	}
 }
